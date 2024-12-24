79ec079716d73d91ecd370a75ce3df3976f4917b
rjoseph757-vs <rjoseph757@gmail.com>
2024-12-24 16:09:24 -0500

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SiteVantagePro_Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SiteVantagePro_Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
+        }
+    }
+}
